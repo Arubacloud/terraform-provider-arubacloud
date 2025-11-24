96b8fc75024a3ccb4ff62bdeca6856a6ff93bf6a
Amedeo Palopoli <amedeo.palopoli@staff.aruba.it>
2025-11-24 18:07:08 +0100

    remove typo

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -7,21 +7,6 @@ terraform {
     
 }
 
-resource "arubacloud_securityrule" "example" {
-  name              = "example-security-rule"
-  location          = "ITBG-Bergamo"
-  project_id        = arubacloud_project.example.id
-  vpc_id            = arubacloud_vpc.example.id
-  security_group_id = arubacloud_securitygroup.example.id
-  properties = {
-    direction = "Ingress"
-    protocol  = "TCP"
-    port      = "80"
-    target = {
-      kind  = "Ip"
-      value = "0.0.0.0/0"
-    }
-  }
-}
+
 
 
