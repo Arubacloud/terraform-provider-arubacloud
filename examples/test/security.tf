b42d3cb6e1076eba186ad31e7fc0946444a53832
Amedeo Palopoli <amedeo.palopoli@staff.aruba.it>
2025-11-25 11:47:04 +0100

    add additional resources

--- examples/test/security.tf
+++ examples/test/security.tf
@@ -1,8 +1,15 @@
 ## KMS Example Resource
 resource "arubacloud_kms" "example" {
-  name          = "example-kms"
-  project_id    = arubacloud_project.example.id
-  location      = "ITBG-Bergamo"
-  tags          = ["security", "encryption"]
+  name           = "example-kms"
+  project_id     = "example-project-id"
+  location       = "eu-central-1"
+  tags           = ["security", "encryption"]
   billing_period = "monthly"
+}
+
+# KMIP Example Resource
+resource "arubacloud_kmip" "example" {
+  name       = "example-kmip"
+  project_id = "example-project-id"
+  kms_id     = arubacloud_kms.example.id
 }
