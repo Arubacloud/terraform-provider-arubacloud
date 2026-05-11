960cd3b365de289a7e27c285c33fe1bde9cd2427
Amedeo Palopoli <amedeo.palopoli@staff.aruba.it>
2026-05-11 14:48:49 +0200

    fix: bump provider version constraint to ~> 0.1.0 in compute test example

--- examples/test/compute/01-provider.tf
+++ examples/test/compute/01-provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    arubacloud = {
+      source  = "arubacloud/arubacloud" # Uses public registry by default
+      version = "~> 0.1.0"              # Use v0.1.x or override with local build via .terraformrc
+    }
+  }
+}
+
+
 provider "arubacloud" {
   api_key    = var.arubacloud_api_key
   api_secret = var.arubacloud_api_secret
--- examples/test/compute/03-main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    arubacloud = {
-      source  = "arubacloud/arubacloud" # Uses public registry by default
-      version = "~> 0.0.1"              # Use v0.0.1 or override with local build via .terraformrc
-    }
-  }
-}
