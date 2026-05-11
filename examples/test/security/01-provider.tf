2e021f2cbf1398d8d8d8e0dd6f9870d8369af121
Amedeo Palopoli <amedeo.palopoli@staff.aruba.it>
2026-05-11 14:44:14 +0200

    fix: bump provider version constraint to ~> 0.1.0 in test examples

--- examples/test/security/01-provider.tf
+++ examples/test/security/01-provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     arubacloud = {
       source  = "arubacloud/arubacloud" # Uses public registry by default
-      version = "~> 0.0.1"              # Use v0.0.1 or override with local build via .terraformrc
+      version = "~> 0.1.0"              # Use v0.1.x or override with local build via .terraformrc
     }
   }
 }
--- examples/test/wordpress/01-provider.tf
+++ examples/test/wordpress/01-provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     arubacloud = {
       source  = "arubacloud/arubacloud"
-      version = "~> 0.0.1"
+      version = "~> 0.1.0"
     }
   }
 }
